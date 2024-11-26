d9166e74932f9198c8132ee776ae99ed4c04cd58
kanzano <kanzano0403@gmail.com>
2024-11-26 18:31:03 +0000

    Fixed spelling mistake. Added Django reloader. Added Requirements.txt

--- MUSIC/settings.py
+++ MUSIC/settings.py
@@ -37,13 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "django_browser_reload",
     # added
     'static',
     'store',
     'accounts',
     'products',
     'cart',
-    'seach',
+    'search',
 ]
 
 MIDDLEWARE = [
@@ -54,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django_browser_reload.middleware.BrowserReloadMiddleware",
 ]
 
 ROOT_URLCONF = 'MUSIC.urls'
