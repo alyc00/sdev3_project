d827755e621093104300c65df87fad3b0cbd3640
kanzano <kanzano0403@gmail.com>
2024-11-27 19:59:03 +0000

    Created basic account functions

--- MUSIC/settings.py
+++ MUSIC/settings.py
@@ -110,6 +110,8 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+
+
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
@@ -131,3 +133,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.CustomUser'
--- accounts/models.py
+++ accounts/models.py
@@ -1,17 +1,25 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 
 # Create your models here.
 
 class Profile(models.Model):
     user = models.OneToOneField(
-        get_user_model(),
+        # get_user_model(),
+         settings.AUTH_USER_MODEL,
         null=True,
         on_delete=models.CASCADE,
     )
 
     def __str__(self):
-        return str(self)
+        return str(self.user)
     
+class CustomUser(AbstractUser):
+    fname = models.CharField(max_length=200)
+    lname = models.CharField(max_length=200)
     
+    class Meta:
+        verbose_name = "Custom User"
+        verbose_name_plural = "Custom Users"
