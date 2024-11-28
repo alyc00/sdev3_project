ea8734283cbd8c77a8b8a5a45c497bcac0c577ea
Alex Cantwell <alexcantwell00@gmail.com>
2024-11-28 16:33:37 +0000

    Add files via upload

--- products/admin.py
+++ products/admin.py
@@ -1,15 +1,3 @@
 from django.contrib import admin
-from .models import Genre, Product
 
-
-class GenreAdmin(admin.ModelAdmin):
-    list_display = ['genre_name']
-
-admin.site.register(Genre, GenreAdmin)
-
-
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ['album_name', 'artist_name', 'genre', 'release_year', 'price', 'available', 'created', 'updated']
-    list_editable = ['price', 'available']
-
-admin.site.register(Product, ProductAdmin)
+# Register your models here.
--- products/views.py
+++ products/views.py
@@ -1,32 +1,3 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Genre, Product
-from django.core.paginator import Paginator, EmptyPage, InvalidPage
+from django.shortcuts import render
 
-def prod_list(request, genre_id=None):
-    genre = None
-    products = Product.objects.filter(available=True)
-
-    if genre_id:
-        genre = get_object_or_404(Genre, id=genre_id)
-        products = Product.objects.filter(genre=genre, available=True)
-    
-    paginator = Paginator(products, 25)
-    
-    try:
-        page = int(request.GET.get('page', '1'))
-    except:
-        page = 1
-    
-    try:
-        products = paginator.page(page)
-    except (EmptyPage, InvalidPage):
-        products = paginator.page(paginator.num_pages)
-    
-    return render(request, '<LINK>.html',{'genre':genre, 'products':products})
-    
-
-def product_detail(request, genre_id, product_id):
-    product = get_object_or_404(Product, genre_id=genre_id, id=product_id)
-    
-    return render(request, '<LINK>.html', {'product':product})
- 
+# Create your views here.
